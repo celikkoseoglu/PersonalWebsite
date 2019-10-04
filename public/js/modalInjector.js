af16c852b7872e5cb5515ed583d0ca28349b3d58
Celik Koseoglu <celikkoseoglu@yahoo.com>
2019-10-04 19:38:22 +0100

    cleaned up some duplicate code

--- public/js/modalInjector.js
+++ public/js/modalInjector.js
@@ -1,4 +1,4 @@
-var modalTemplate = '<div class=\"modal fade\" id=\"#modal-id\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\"><div class=\"modal-dialog\"><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\"></button> <h4 class=\"modal-title\">#modal-title</h4></div><div class=\"modal-body row\"><img style=\"margin-top: 70px;\" src=\"images/#image-filename\" name=\"aboutme\" class=\"img-responsive col-md-4\"/><h3 class=\"media-heading\">#app-name</h3><h4 class=\"text-muted\">#header</h4><p>#description</p><hr><span><strong>#list-1-text</strong></span>#technologies-used<br><span><strong>#list-2-text</strong></span>#download-for<hr><span><strong>#list-3-text</strong></span><br>#image-links</div><div class=\"modal-footer\"><center><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></center></div></div></div></div>';
+const modalTemplate = '<div class=\"modal fade\" id=\"#modal-id\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\"><div class=\"modal-dialog\"><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\"></button> <h4 class=\"modal-title\">#modal-title</h4></div><div class=\"modal-body row\"><img style=\"margin-top: 70px;\" src=\"images/#image-filename\" name=\"aboutme\" class=\"img-responsive col-md-4\"/><h3 class=\"media-heading\">#app-name</h3><h4 class=\"text-muted\">#header</h4><p>#description</p><hr><span><strong>#list-1-text</strong></span>#technologies-used<br><span><strong>#list-2-text</strong></span>#download-for<hr><span><strong>#list-3-text</strong></span><br>#image-links</div><div class=\"modal-footer\"><center><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></center></div></div></div></div>';
 
 var imageTemplate = '<img src=\"#link\"height=\"120\"/>';
 var labelTemplate = '<span class=\"label label-#type\">#inner-text</span>';
@@ -12,7 +12,7 @@ function injectModal(newModal) {
 }
 
 function createModal(modalId, appName, category, description, logoLocation, slogan, screenshots, list1Content, list2Content, list1Text, list2Text, list3Text) {
-    var newModal = modalTemplate;
+    let newModal = modalTemplate;
     newModal = newModal.replace('#modal-id', modalId);
     newModal = newModal.replace('#modal-title', category);
     newModal = newModal.replace('#image-filename', logoLocation);
@@ -20,22 +20,22 @@ function createModal(modalId, appName, category, description, logoLocation, slog
     newModal = newModal.replace('#app-name', appName);
     newModal = newModal.replace('#header', slogan);
 
-    var imageLinks = '';
+    let imageLinks = '';
 
-    for (var i in screenshots) {
+    for (let i in screenshots) {
         imageLinks += (imageLinks, this.imageTemplate.replace('#link', screenshots[i]));
     }
 
     newModal = newModal.replace('#image-links', imageLinks);
 
-    var techUsed = '';
-    var downFor = '';
+    let techUsed = '';
+    let downFor = '';
 
-    for (var i = 0; i < list1Content.length; i++) {
+    for (let i = 0; i < list1Content.length; i++) {
         techUsed += (techUsed, this.labelTemplate.replace('#inner-text', list1Content[i][0]).replace('#type', list1Content[i][1]))
     }
 
-    for (var i = 0; i < list2Content.length; i++) {
+    for (let i = 0; i < list2Content.length; i++) {
         downFor += (downFor, this.labelTemplate.replace('#inner-text', list2Content[i][0]).replace('#type', list2Content[i][1]))
     }
 
@@ -52,7 +52,7 @@ function createModal(modalId, appName, category, description, logoLocation, slog
 //requests the .json object from the server
 function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
+    let xobj = new XMLHttpRequest();
     xobj.overrideMimeType('application/json');
     xobj.open('GET', 'js/modals.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
@@ -67,10 +67,10 @@ function loadJSON(callback) {
 function init() {
     loadJSON(function (response) {
         // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
+        let actual_JSON = JSON.parse(response);
 
-        for (var i = 0; i < actual_JSON.length; i++) {
-            var current = actual_JSON[i];
+        for (let i = 0; i < actual_JSON.length; i++) {
+            let current = actual_JSON[i];
             createModal(current.modalId, current.title, current.category, current.description, current.logoLocation, current.slogan, current.screenshots, current.list_1_content, current.list_2_content, current.list_1_text, current.list_2_text, current.list_3_text);
         }
     });
